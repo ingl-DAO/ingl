a71627d862f1615b5b208b2ce1b0830b33e6917c
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-07-25 11:38:30 +0100

    added the page layout for the ingl app

---
+++ apps/app/src/components/layout/Navbar.tsx
@@ -0,0 +1,141 @@
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Logo from '../../assets/logo.png';
+import theme from '../../theme/theme';
+import { Button, IconButton, SwipeableDrawer, Tooltip } from '@mui/material';
+import NavItem, { ExternalNavItem } from './NavItem';
+import { MenuRounded } from '@mui/icons-material';
+
+const NAV_LINKS: { name: string; link: string; isExternal: boolean }[] = [
+  {
+    name: 'NFTS',
+    link: '/nft',
+    isExternal: false,
+  },
+  { name: 'DAO', link: '/dao', isExternal: false },
+  { name: 'Wallet', link: '/wallet', isExternal: false },
+  { name: 'Onboard Validator', link: '/onboard', isExternal: false },
+];
+
+export default function Navbar() {
+  const [isSideBarOpen, setIsSideBarOpen] = useState<boolean>(false);
+  const toggleDrawer = (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+
+    setIsSideBarOpen(!isSideBarOpen);
+  };
+
+  const SideNav = () => (
+    <Box
+      sx={{
+        width: 250,
+        backgroundColor: 'black',
+        height: '100%',
+        paddingTop: theme.spacing(5),
+        display: 'grid',
+        rowGap: theme.spacing(5),
+        gridTemplateRows: 'auto auto auto auto 1fr',
+      }}
+      role="presentation"
+      onClick={toggleDrawer}
+      onKeyDown={toggleDrawer}
+    >
+      {NAV_LINKS.map(({ isExternal, link, name }, index) =>
+        !isExternal ? (
+          <NavItem
+            to={link}
+            sx={{ fontSize: { mobile: '0.8rem', laptop: '1.25rem' } }}
+            key={index}
+          >
+            {name}
+          </NavItem>
+        ) : (
+          <ExternalNavItem link={{ link, name }} />
+        )
+      )}
+    </Box>
+  );
+
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: '#10141e',
+          display: 'grid',
+          gridTemplateColumns: {
+            laptop: 'auto 1fr auto',
+            mobile: '1fr auto',
+          },
+          alignItems: 'center',
+          px: theme.spacing(8),
+          py: theme.spacing(2.375),
+        }}
+      >
+        <Box sx={{ height: { laptop: '59.88px', mobile: '40px' } }}>
+          <img src={Logo} height="100%" alt="ingl logo" />
+        </Box>
+        <Box
+          sx={{
+            display: { laptop: 'grid', mobile: 'none' },
+            gridTemplateColumns: 'auto auto auto auto',
+            columnGap: theme.spacing(10),
+            justifyContent: 'center',
+          }}
+        >
+          {NAV_LINKS.map(({ isExternal, link, name }, index) =>
+            !isExternal ? (
+              <NavItem
+                to={link}
+                sx={{ fontSize: { mobile: '0.8rem', laptop: '1.25rem' } }}
+                key={index}
+              >
+                {name}
+              </NavItem>
+            ) : (
+              <ExternalNavItem link={{ link, name }} />
+            )
+          )}
+        </Box>
+        <Button
+          color="primary"
+          variant="contained"
+          component="a"
+          href="https://app.ingl.io/mint"
+          rel="noreferrer"
+          target="_blank"
+          sx={{
+            borderRadius: '90px',
+            justifySelf: 'end',
+            display: { laptop: 'initial', mobile: 'none' },
+          }}
+        >
+          Get Started
+        </Button>
+        <IconButton
+          onClick={toggleDrawer}
+          sx={{ display: { laptop: 'none', mobile: 'initial' } }}
+        >
+          <Tooltip arrow title="menu">
+            <MenuRounded color="secondary" />
+          </Tooltip>
+        </IconButton>
+      </Box>
+
+      <SwipeableDrawer
+        anchor="left"
+        open={isSideBarOpen}
+        onClose={toggleDrawer}
+        onOpen={toggleDrawer}
+      >
+        <SideNav />
+      </SwipeableDrawer>
+    </>
+  );
+}
--- apps/app/src/routes/routes.tsx
+++ apps/app/src/routes/routes.tsx
@@ -1,10 +1,16 @@
 import { Navigate } from 'react-router';
+import Layout from '../components/layout';
 import Test from '../components/Test';
 
 export const routes = [
   {
     path: '/',
-    element: <Test />,
+    element: <Layout />,
+    children: [
+      { path: '/', element: <Navigate to="/nft" /> },
+      { path: '/nft', element: <Test /> },
+      { path: '*', element: <Navigate to="/" /> },
+    ],
   },
   { path: '*', element: <Navigate to="/" /> },
 ];
