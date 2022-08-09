bf25c119ea420b2b3aa92fef2c1902df855ad012
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2022-08-09 21:20:35 +0100

    finished working on validators

---
+++ apps/app/src/components/dao/inglNumber.tsx
@@ -0,0 +1,33 @@
+import { Box, Skeleton, Typography } from '@mui/material';
+import React from 'react';
+import { InglSummary } from '.';
+import shortenNumber from '../../common/utils/shortenNumber';
+import theme from '../../theme/theme';
+
+export default function InglNumber({
+  data: { displayTitle, amount },
+  isDataLoading,
+}: {
+  data: InglSummary;
+  isDataLoading: boolean;
+}) {
+  return (
+    <Box
+      sx={{
+        backgroundColor: theme.palette.primary.main,
+        borderRadius: theme.spacing(3),
+        padding: theme.spacing(2),
+      }}
+    >
+      <Typography sx={{textAlign: 'center'}}>{displayTitle}</Typography>
+      {isDataLoading ? (
+        <Skeleton
+          variant="text"
+          sx={{ backgroundColor: 'rgba(177,177,177,0.17)' }}
+        />
+      ) : (
+        <Typography sx={{fontWeight: 'bold', fontSize: '3rem', textAlign:'center'}}>{shortenNumber(amount)}</Typography>
+      )}
+    </Box>
+  );
+}
